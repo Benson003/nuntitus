5d579588206aab2f07333b35af0829624b3afbc2
Nwankwo Onyedika Benson <134145724+Benson003@users.noreply.github.com>
2025-07-02 12:11:31 +0100

    Blog Builder
    Created the blog builder to handle blog objects in creating and updating

--- backend/database/blog_builder.go
+++ backend/database/blog_builder.go
@@ -1,1 +1,48 @@
 package database
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type BlogBuilder struct {
+	Blog BlogTable
+}
+
+func NewBlogBuilder() BlogBuilder {
+	return BlogBuilder{
+		Blog: BlogTable{},
+	}
+}
+
+func (b *BlogBuilder) FetchBlogToUpdate(db *gorm.DB, user_id uuid.UUID, blog_id uuid.UUID) (*BlogBuilder, error) {
+	var blog BlogTable
+	if err := db.Where("user_id = ? AND blog_id = ?", user_id, blog_id).First(&blog).Error; err != nil {
+		return &BlogBuilder{}, err
+	}
+	return &BlogBuilder{Blog: blog}, nil
+}
+
+func (b *BlogBuilder) SetTitle(title string) *BlogBuilder {
+	b.Blog.Title = title
+	return b
+}
+func (b *BlogBuilder) SetSummary(summary string) *BlogBuilder {
+	b.Blog.Summary = summary
+	return b
+}
+
+func (b *BlogBuilder) SetVisiblity(visibility bool) *BlogBuilder {
+	b.Blog.Visibility = visibility
+	return b
+}
+func (b *BlogBuilder) SetPublishTime(publish_time time.Time) *BlogBuilder {
+	b.Blog.PublishTime = publish_time
+	return b
+}
+
+func (b *BlogBuilder) Build() BlogTable {
+	return b.Blog
+}
--- backend/database/blog_table.go
+++ backend/database/blog_table.go
@@ -1,11 +1,11 @@
 package database
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
-
-
 
 
 
@@ -14,3 +14,8 @@ func (b *BlogTable) BeforeCreate(tx *gorm.DB)(Error error){
 	b.BlogID = uuid.Must(uuid.NewRandom())
 	return nil
 }
+func (u *UserTable) AfterUpdate(tx *gorm.DB) (Error error) {
+	u.UpdatedAt = time.Now()
+	return nil
+}
+
