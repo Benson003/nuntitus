9977e65f1caf6bdfdb23a0f220e7b6c6f8b45b03
Nwankwo Onyedika Benson <134145724+Benson003@users.noreply.github.com>
2025-07-02 22:18:09 +0100

    Made An un intentional wipe

--- backend/database/blog_table.go
+++ backend/database/blog_table.go
@@ -7,15 +7,26 @@ import (
 	"gorm.io/gorm"
 )
 
-
-
-
-func (b *BlogTable) BeforeCreate(tx *gorm.DB)(Error error){
+func (b *BlogTable) BeforeCreate(tx *gorm.DB) (Error error) {
 	b.BlogID = uuid.Must(uuid.NewRandom())
 	return nil
 }
-func (u *UserTable) AfterUpdate(tx *gorm.DB) (Error error) {
+func (u *BlogTable) AfterUpdate(tx *gorm.DB) (Error error) {
 	u.UpdatedAt = time.Now()
 	return nil
 }
 
+
+func (db *DBObject) CreateBlog(user_id uuid.UUID,title string, summary string) error{
+
+	return nil
+}
+
+
+func (db *DBObject) GetBlogs(user_id uuid.UUID, page int, count int) ([]BlogTable, error) {
+	return []BlogTable{}, nil
+}
+
+func (db *DBObject) GetBlogByID(user_id uuid.UUID, blog_id uuid.UUID) (BlogTable, error) {
+	return BlogTable{}, nil
+}
--- backend/database/user_table.go
+++ backend/database/user_table.go
@@ -2,57 +2,74 @@ package database
 
 import (
 	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
+// This ensures the created at always has a date
 func (u *UserTable) BeforeCreate(tx *gorm.DB) (Error error) {
 	u.UserID = uuid.Must(uuid.NewRandom())
 	return nil
 }
 
+// If any record
 func (u *UserTable) AfterUpdate(tx *gorm.DB) (Error error) {
 	u.UpdatedAt = time.Now()
 	return nil
 }
 
-func (db *DBObject) CreateUser(username string,hashed_password string,email string)(UserTable,error){
+func (db *DBObject) CreateUser(username string, hashed_password string, email string) (UserTable, error) {
 	user := NewUserBuilder()
 	user.SetUsername(username).SetEmail(email).SetPasswordHash(hashed_password)
 	f_user := user.Build()
 
-	if err := db.DB.Create(&f_user).Error;err != nil{
-		return f_user,err
+	if err := db.DB.Create(&f_user).Error; err != nil {
+		return f_user, err
 	}
-	return f_user,nil
+	return f_user, nil
 }
 
-func (db *DBObject)GetUserByID(user_id uuid.UUID)(UserTable, error){
+func (db *DBObject) GetUserByID(user_id uuid.UUID) (UserTable, error) {
 	var user UserTable
-	if err := db.DB.Where("user_id = ?",user_id).First(&user).Error; err != nil{
-		return user,err
+	if err := db.DB.Where("user_id = ?", user_id).First(&user).Error; err != nil {
+		return user, err
 	}
-	return user,nil
+	return user, nil
 }
 
-func (db *DBObject) DeleteUser(user_id uuid.UUID)(UserTable ,error){
-	user ,err := db.GetUserByID(user_id)
-	if err != nil{
-		return user,err
+func (db *DBObject) GetUserByEmail(email string) (UserTable, error) {
+	var user UserTable
+	if err := db.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return user, err
 	}
-	if err := db.DB.Delete(&user).Error; err != nil{
-		return user,err
+	return user, nil
+}
+func (db *DBObject) GetUserByUsername(username string) (UserTable, error) {
+	var user UserTable
+	if err := db.DB.Where("username = ?", username).First(&user).Error; err != nil {
+		return user, err
 	}
-	return user,nil
+	return user, nil
 }
 
-func (db *DBObject)UpdateUser(user_id uuid.UUID, username string , password_hash string, email string)(User UserTable,Error error){
+func (db *DBObject) DeleteUser(user_id uuid.UUID) (UserTable, error) {
+	user, err := db.GetUserByID(user_id)
+	if err != nil {
+		return user, err
+	}
+	if err := db.DB.Delete(&user).Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (db *DBObject) UpdateUser(user_id uuid.UUID, username string, password_hash string, email string) (UserTable, error) {
 	userBuilder := NewUserBuilder()
 
-
-	user,err := userBuilder.FetchUserToUpdate(db.DB,user_id)
-	if  err != nil {
-		return UserTable{},err
+	user, err := userBuilder.FetchUserToUpdate(db.DB, user_id)
+	if err != nil {
+		return UserTable{}, err
 	}
 	if username != "" {
 		user.SetUsername(username)
@@ -64,5 +81,10 @@ func (db *DBObject)UpdateUser(user_id uuid.UUID, username string , password_hash
 	if email != "" {
 		user.SetEmail(email)
 	}
-	return user.Build(),nil
+	f_user := user.Build()
+	if err := db.DB.Save(&f_user).Error; err != nil {
+		return UserTable{}, err
+	}
+
+	return f_user, nil
 }
